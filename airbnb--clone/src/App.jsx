b78098280a0fe833e6f9feab75ad0b5a0e189034
Precious Gartrell <precious.gartrell@allovue.com>
2024-03-23 22:47:35 -0400

    using map

--- airbnb--clone/src/App.jsx
+++ airbnb--clone/src/App.jsx
@@ -2,17 +2,31 @@ import './App.css'
 import Cards from './cards.jsx'
 import Hero from './hero.jsx'
 import Navbar from './navbar.jsx'
+import Data from './data.js'
 
 
 
 function App() {
-
   return (
     <>
   <Navbar />
   <Hero />
-  <Cards />
-  
+  <section className='cards--list'>
+      { Data.map(
+        x => (
+
+        <Cards
+          key={x.id}
+          img ={x.coverImg}
+          rating= {x.stats.rating}
+          reviewCount={x.stats.reviewCount}
+          country="USA"
+          title={x.title}
+          price={x.price}
+        />
+        )
+      )}
+  </section>
     </>
   )
 }
--- airbnb--clone/src/cards.jsx
+++ airbnb--clone/src/cards.jsx
@@ -1,19 +1,21 @@
 import './cardscss.css'
 
-export default function Cards(){
+export default function Cards(props){
+
+    
     return(
         <div className='card--maindiv'>
-            <img className="card--photo" src="/swimmer.png" alt="" height="235px" width="176px"/>
+            <img className="card--photo" src={props.img} alt="" height="235px" width="176px"/>
             <div className='card--photoText'>SOLD OUT</div>
             <div className='card--infodiv'>
                 <div className='card--ratingdiv'>
                     <img className='card--starIcon' src='/star-icon.png' height="30px" width='30px' />
-                    <p className='card--rating'>5.0</p>
-                    <p className='card--rating2'>(6) USA</p>
+                    <p className='card--rating'>{props.rating}</p>
+                    <p className='card--rating2'>{props.reviewCount} {props.country}</p>
                 </div>
-                <h2 className='card--summary'>Learn Lessons with Katie Zaferes</h2>
+                <h2 className='card--summary'>{props.title}</h2>
                 <div className='card--pay'>
-                    <p className='card--summarypay1'>From $136 /</p>
+                    <p className='card--summarypay1'>From ${props.price} /</p>
                     <p className='card--summarypay2'> person</p>            
                 </div>
             </div>
